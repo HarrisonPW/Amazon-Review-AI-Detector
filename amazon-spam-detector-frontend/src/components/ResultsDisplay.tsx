d46b270e4ad2ded03c4a9378c24ec76e99c5e1ef
Jiayue Meng <90874903+JiayueMeng@users.noreply.github.com>
2024-11-14 22:17:13 -0500

    add gemini to frontend

--- amazon-spam-detector-frontend/src/components/ResultsDisplay.tsx
+++ amazon-spam-detector-frontend/src/components/ResultsDisplay.tsx
@@ -5,7 +5,7 @@ interface ResultsDisplayProps {
     results: {
         result_gpt2: string;
         result_nb: string;
-        result_nb2: string;
+        result_gemini: string;
         result_nb3: string;
         result_nb4: string;
         spam_percentage: number;
@@ -13,7 +13,7 @@ interface ResultsDisplayProps {
 }
 
 const ResultsDisplay: React.FC<ResultsDisplayProps> = ({results}) => {
-    const {result_gpt2, result_nb, result_nb2, result_nb3, result_nb4, spam_percentage} = results;
+    const {result_gpt2, result_nb, result_gemini, result_nb3, result_nb4, spam_percentage} = results;
 
     return (
         <div className="results-display">
@@ -21,7 +21,7 @@ const ResultsDisplay: React.FC<ResultsDisplayProps> = ({results}) => {
             <ul>
                 <li>GPT-2 Model: {result_gpt2}</li>
                 <li>MultinomialNB Model 1: {result_nb}</li>
-                <li>MultinomialNB Model 2: {result_nb2}</li>
+                <li>Gemini-1.5 Model: {result_gemini}</li>
                 <li>MultinomialNB Model 3: {result_nb3}</li>
                 <li>MultinomialNB Model 4: {result_nb4}</li>
             </ul>
